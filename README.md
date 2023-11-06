658a51c5938eeaa5936338b9ceab049c80030feb
rob <robertcasson11@gmail.com>
2023-11-06 22:43:24 +0000

    README.md created -- 404 routes added to server

---
+++ README.md
@@ -0,0 +1,5 @@
+# Auth Template
+
+## A reusable server app to wrap any protfolio sites with a login.
+
+The app will authenticate the user as someone who has access to the site and block anyone else.
--- server.js
+++ server.js
@@ -6,4 +6,15 @@ app.get("/", (req, res) => {
   res.send("hey");
 });
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.send("No page exists");
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
 app.listen(PORT, () => console.log("Listening on port 3000"));
